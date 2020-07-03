e17718cf861624df0047c37aa393507e023eb75a
nishat1 <nishatgupta1@gmail.com>
2020-07-03 01:21:41 -0700

    Updates

---
+++ precache-manifest.60bb3e37ad4c1e6c448e168b3bcc8ef4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1760d8c3273a79fa3102ba1fdb2f199b",
+    "url": "/theniche/index.html"
+  },
+  {
+    "revision": "f0e5aa0133d7644af470",
+    "url": "/theniche/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e538f7e362efcf71e440",
+    "url": "/theniche/static/js/2.f6c1c698.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theniche/static/js/2.f6c1c698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e5aa0133d7644af470",
+    "url": "/theniche/static/js/main.17d8e1ab.chunk.js"
+  },
+  {
+    "revision": "9ba7e3cce8cf7d45cc8e",
+    "url": "/theniche/static/js/runtime-main.007c7667.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/theniche/static/media/logo.5d5d9eef.svg"
+  }
+]);
